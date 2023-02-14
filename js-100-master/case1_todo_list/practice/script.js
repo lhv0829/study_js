f8416ab953fa50377ab515061c635dedb393ae6f
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-02-15 04:32:36 +0900

    좀..해보긴했는데 우선 강의를 다 본 후에 뜯어보자..

--- js-100-master/case1_todo_list/practice/script.js
+++ js-100-master/case1_todo_list/practice/script.js
@@ -5,6 +5,11 @@
     return document.querySelector(target)
   }
 
+  const API_URL = 'http://localhost:3000/todos'
+  const $todos = get('.todos');
+  const $form = get('.todo_form');
+  const $todoInput = get('.todo_input');
+
   const createTodoElement = (item) => {
     const { id, content } = item
     const $todoItem = document.createElement('div')
@@ -39,6 +44,61 @@
     return $todoItem
   }
 
-  const init = () => {}
+  const renderAllTodos = (todos) => {
+    $todos.innerHTML = '';
+    todos.forEach((item) => {
+      const todoElement = createTodoElement(item);
+      $todos.appendChild(todoElement);
+    });
+  };
+
+  const getTodos = () => {
+    fetch(API_URL)
+    .then((response) => response.json())
+    .then(todos => renderAllTodos(todos))
+    .catch((error) => console.error(error));
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const todo = {
+      content: $todoInput.value,
+      completed: false,
+    };
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    }).then(getTodos).then(() => {
+      $todoInput.value = '';
+      $todoInput.focus();
+    }).catch((error) => console.log(error))
+  };
+
+  const toggleTodo = () => {
+    if(e.target.className !== 'todo_checkbox') return;
+    const $item = e.target.closet('.item');
+    const id = $item.dataset.id;
+    const completed = e.target.checked;
+
+    fetch(`${API_URL}/${id}`, {
+      method: 'PATCH', // 부분만 변경. PUT은 전체 변경
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({completed}),
+    }).then(getTodos)
+    .catch((error) => console.log(error))
+  };
+
+  const init = () => {
+    window.addEventListener('DOMContentLoaded', () => {
+      getTodos();
+    })
+    $form.addEventListener('submit', addTodo);
+    $todos.addEventListener('click', toggleTodo);
+  };
   init()
 })()
